7cd76f8669fe05d12bbe792f6aa0d1a991bc9691
JCxYIS <jsn811@gmail.com>
2020-04-23 16:35:07 +0800

    5-1 improvv

--- 5-1(極座標畫圖).py
+++ 5-1(極座標畫圖).py
@@ -12,7 +12,7 @@ angs = linspace(a, b, n)
 
 rs = sin(8*angs/7)
 
-pylab.polar( angs, rs, lw=5, label="SIN+COS" )
+pylab.polar( angs, rs, lw=5, label="8theta/7" )
 
 pylab.title("Polar Bear!", color='g')
 # pylab.legend()
