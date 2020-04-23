12aeda2f62aeefb2d1be1efcef1b909ed0948227
JCxYIS <jsn811@gmail.com>
2020-04-23 16:34:33 +0800

    HW 5-1

---
+++ 5-1(極座標畫圖).py
@@ -0,0 +1,22 @@
+import pylab
+from numpy import *
+
+
+# angmin, angmax, splitPoints
+a = 0
+b = 14*pi
+n = 8763
+
+
+angs = linspace(a, b, n)
+
+rs = sin(8*angs/7)
+
+pylab.polar( angs, rs, lw=5, label="SIN+COS" )
+
+pylab.title("Polar Bear!", color='g')
+# pylab.legend()
+# pylab.grid()
+
+
+pylab.show()
